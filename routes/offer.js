3e61c3aba06f174032a9dd8adb9272b77e9d9ee8
GeremyColley <78615407+GeremyColley@users.noreply.github.com>
2023-07-06 16:05:37 +0200

    update offer middleware

--- routes/offer.js
+++ routes/offer.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const isAuthenticated = require("../middleware/isAuthenticated");
 
 const Offer = require('../models/Offer');
 const User = require("../models/User");
